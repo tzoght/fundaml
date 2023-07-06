5a8a9f1d6a03c917470d4289f2ad2571e8130ae9
Tony Zoght <tony@zoght.com>
2023-07-05 21:52:42 -0700

    debugging

--- tests/test_trainer_base.py
+++ tests/test_trainer_base.py
@@ -28,27 +28,27 @@ class TestTrainerBase(unittest.TestCase):
             transform=ToTensor()
         )
 
-        learning_rate = 1e-3
-        batch_size = 64
-        epochs = 1
-        weight_decay = 0.01
+        # learning_rate = 1e-3
+        # batch_size = 64
+        # epochs = 1
+        # weight_decay = 0.01
         
-        train_dataloader = DataLoader(training_data, batch_size=batch_size)
-        test_dataloader = DataLoader(test_data, batch_size=batch_size)        
+        # train_dataloader = DataLoader(training_data, batch_size=batch_size)
+        # test_dataloader = DataLoader(test_data, batch_size=batch_size)        
         
-        model = SampleNNClassifier(short_name="Sample_NN_Classifier")
-        loss_fn = nn.CrossEntropyLoss()
-        trainer = NNTrainer()
+        # model = SampleNNClassifier(short_name="Sample_NN_Classifier")
+        # loss_fn = nn.CrossEntropyLoss()
+        # trainer = NNTrainer()
         
-        # optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-        optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay, betas=(0.9, 0.99), eps=1e-8)
+        # # optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+        # optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay, betas=(0.9, 0.99), eps=1e-8)
 
-        trainer.with_model(model).with_optimizer(optimizer).with_loss_function(loss_fn)
-        trainer.with_scoring_functions({'accuracy':score_accuracy})
-        # scores = trainer.train_loop(train_dataloader,update_every_n_batches=20,epochs=epochs)
-        # scores = trainer.test_loop(test_dataloader,update_every_n_batches=2)
-        scores = trainer.train(train_dataloader, test_dataloader,update_every_n_batches=100, epochs=5)
-        print(scores)
+        # trainer.with_model(model).with_optimizer(optimizer).with_loss_function(loss_fn)
+        # trainer.with_scoring_functions({'accuracy':score_accuracy})
+        # # scores = trainer.train_loop(train_dataloader,update_every_n_batches=20,epochs=epochs)
+        # # scores = trainer.test_loop(test_dataloader,update_every_n_batches=2)
+        # scores = trainer.train(train_dataloader, test_dataloader,update_every_n_batches=100, epochs=5)
+        # print(scores)
         
 if __name__ == '__main__':
     unittest.main()
