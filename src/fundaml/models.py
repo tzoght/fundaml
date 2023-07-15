315e7f3f6879ce43c2f51616e0f215c1d791da05
Tony Zoght <tony@zoght.com>
2023-07-15 15:29:56 -0700

    Rnn cnn sample model (#10)
    * add sample rnn and cnn models
    
    * add working sample rnn and cnn
    
    * add training loop for rnn and cnn
    
    * changing naming of rnn and cnn sample
    
    * fix model names in test script
    
    ---------
    
    Co-authored-by: Yuri <91753999+Yurui-Feng@users.noreply.github.com>

--- src/fundaml/models.py
+++ src/fundaml/models.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from abc import ABC, abstractmethod
 from torch.nn import functional as F
@@ -44,3 +45,58 @@ class SampleNNClassifier(AbstractNN):
         logits = self.linear_relu_stack(x)
         
         return logits
+
+class SampleCNN(AbstractNN):
+    """
+    A sample Convolutional Neural Network (CNN) model.
+    """
+    def __init__(self, short_name):
+        super(SampleCNN, self).__init__(short_name)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.flatten = nn.Flatten() 
+        self.fc1 = nn.Linear(12544, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        output = self.fc2(x)
+        return output
+
+
+class SampleRNN(AbstractNN):
+    """
+    A sample Recurrent Neural Network (RNN) model.
+    """
+    def __init__(self, short_name, input_size=28, hidden_size=128, num_layers=2, num_classes=10):
+        super(SampleRNN, self).__init__(short_name)
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+    
+    def forward(self, x):
+        # Set initial hidden and cell states 
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+
+        # Reshape input to (batch_size, sequence_length, input_size)
+        x = x.view(x.size(0), -1, self.input_size)
+
+        # Forward propagate RNN
+        out, _ = self.rnn(x, h0)
+
+        # Decode the hidden state of the last time step
+        out = self.fc(out[:, -1, :])
+        return out
--- tests/test_trainer_base.py
+++ tests/test_trainer_base.py
@@ -1,4 +1,4 @@
-from fundaml.models import SampleNNClassifier
+from fundaml.models import SampleNNClassifier, SampleCNN, SampleRNN
 from fundaml.trainers import NNTrainer, get_available_devices
 from fundaml.scores import score_accuracy
 import unittest
@@ -36,20 +36,24 @@ class TestTrainerBase(unittest.TestCase):
         train_dataloader = DataLoader(training_data, batch_size=batch_size)
         test_dataloader = DataLoader(test_data, batch_size=batch_size)        
         
-        model = SampleNNClassifier(short_name="Sample_NN_Classifier")
-        loss_fn = nn.CrossEntropyLoss()
-        trainer = NNTrainer()
+        models = [SampleNNClassifier("Sample_NN_Classifier"), SampleCNN("Sample_CNN"), SampleRNN("Sample_RNN")]
         
-        # optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-        optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay, betas=(0.9, 0.99), eps=1e-8)
+        for model in models:
+            loss_fn = nn.CrossEntropyLoss()
+            trainer = NNTrainer()
+            
+            optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay, betas=(0.9, 0.99), eps=1e-8)
 
-        print(f"Available devices on this machine: {get_available_devices()}")
-        trainer.with_model(model).with_optimizer(optimizer).with_loss_function(loss_fn)
-        trainer.with_scoring_functions({'accuracy':score_accuracy}).with_device('cpu')
-        # scores = trainer.train_loop(train_dataloader,update_every_n_batches=20,epochs=epochs)
-        # scores = trainer.test_loop(test_dataloader,update_every_n_batches=2)
-        scores = trainer.train_test_loop(train_dataloader, test_dataloader,update_every_n_batches=100, epochs=5)
-        # print(scores)
+            print(f"Available devices on this machine: {get_available_devices()}")
+            trainer.with_model(model).with_optimizer(optimizer).with_loss_function(loss_fn)
+            trainer.with_scoring_functions({'accuracy':score_accuracy}).with_device('cpu')
+            # scores = trainer.train_loop(train_dataloader,update_every_n_batches=20,epochs=epochs)
+            # scores = trainer.test_loop(test_dataloader,update_every_n_batches=2)
+            scores = trainer.train_test_loop(train_dataloader, test_dataloader,update_every_n_batches=100, epochs=1)
+            # print(scores)
         
 if __name__ == '__main__':
     unittest.main()
+
+
+
